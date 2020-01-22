7f500551b615c1d1fcd24f73a4994f7b1ac7ad79
Colin Hauch <colinhauch@Colins-Mac-mini.local>
2020-01-21 18:07:50 -0700

    Add a directory for the second assignment

---
+++ Homeworks/Homework2/ReadFile.py
@@ -0,0 +1,24 @@
+# /Users/chauch/Documents/College/Undergraduate/Semester8/400B/400B_hauch/Homeworks/Homework2
+
+# import relavent modules
+import numpy as np
+import astropy.units as u
+
+# define the function Read that takes the name of the file as an input
+def Read(filename):
+	# open the file
+	file = open(filename, 'r')
+
+	line1 = file.readline()
+	#line2 = file.readline()
+
+	print(line1)
+
+	# label, value = line1.split()
+	# time = float(value)*10.0*u.Myr
+
+
+
+
+
+
