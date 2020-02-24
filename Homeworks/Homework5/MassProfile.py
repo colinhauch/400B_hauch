5f252eab74858ea50c5f8b25ce70ac46ef24b1cb
Colin Hauch <colin.hauch@gmail.com>
2020-02-24 13:21:18 -0800

    Continued working on Homework 5, started plotting

--- Homeworks/Homework5/MassProfile.py
+++ Homeworks/Homework5/MassProfile.py
@@ -13,6 +13,8 @@ import astropy.units as u
 from astropy.constants import G
 from ReadFile import Read
 from CenterOfMass import CenterOfMass
+import matplotlib.pyplot as plt
+import matplotlib
 
 G = G.to(u.kpc*u.km**2/u.s**2/u.Msun)
 
@@ -26,7 +28,7 @@ class MassProfile:
 		# 		snap -- snapshot number as an integer
 
 		# Create a string to save as the filename
-		# add a string of the filenumber to the value “000” 
+		# add a string of the filenumber to the value “000”
 		ilbl = "000" + str(snap)
 		# remove all but the last 3 digits
 		ilbl = ilbl[-3:]
@@ -76,8 +78,10 @@ class MassProfile:
 		massValues = np.array([])
 
 		for element in radii:
+
 			# for each distance given, find all the particles within that distance
 			index1 = np.where(particleRadii <= element)
+
 
 			particleMasses = self.m[index1]
 
@@ -89,56 +93,158 @@ class MassProfile:
 		return massValues*1e10*u.Msun
 		
 	def MassEnclosedTotal(self, radii):
+		# INPUTS:
+		# 		radii -- array with various radius values (kpc)
+		#
+		# RETURNS:
+		# 		array of total mass enclosed within the radii (Msun)
 
+		# if the galaxy is M33 (which doesn't have a bulge), make an exception
 		if self.gname == "M33":
+
+			# determine mass of each component but not the bulge
 			darkMass  = self.MassEnclosed(1,radii)
 			diskMass  = self.MassEnclosed(2,radii)
 			bulgeMass = 0 * u.Msun
+
+		# otherwise,
 		else:
+			# determine mass of each component
 			darkMass  = self.MassEnclosed(1,radii)
 			diskMass  = self.MassEnclosed(2,radii)
 			bulgeMass = self.MassEnclosed(3,radii)
 
-		return darkMass + diskMass + bulgeMass
+		# return the sum of the masses
+		return darkMass, diskMass, bulgeMass
 
 	def HernquistMass(self,r,a,darkMass):
+		# INPUTS:
+		#		r -- radius
+		# 		a -- scale length used in this profile
+		#		darkMass -- amount of dark matter to be used in the profile
+		#
+		# RETURNS:
+		# 		computed mass enclosed according to the profile
 
 		return darkMass * r**2 / (a+r)**2
 
 	def CircularVelocity(self, particleType, radii):
+		# INPUTS:
+		#		particleType -- Particle Type. Type 1 = Dark Matter, Type 2 = Disk Stars, Type 3 = Bulge Stars
+		# 		radii -- array with various radius values (kpc)
+		#
+		# RETURNS:
+		# 		array circular velocities
 
+		# create an index of the particles of the desired type
 		index = np.where(self.data['type'] == particleType)
 
-
-
+		# Subtract the COM position from each component value for every particle
+		# of the desired type
 		xdists = self.x[index] - COM_P[0].value
 		ydists = self.y[index] - COM_P[1].value
 		zdists = self.z[index] - COM_P[2].value
 
+		# calculate magnitude of position vectors
 		particleRadii = np.sqrt(xdists**2 + ydists**2 + zdists**2)
 
+		# create an empty array
 		circVelos = np.array([])
 
+		# for each radii in particle radii list, 
 		for element in particleRadii:
 
+			# pass that single value radii, as an array, to the MassEnclosed function
+			# which returns an ARRAY. The returned array in this case will be of length 1
 			M = MassEnclosed([element])
 
-			circVelo = np.sqrt(G*M/element)
+			# calculate the circular velocity for that radii
+			circVelo = np.sqrt(G*M[0]/element)
 
+			# append the calculated circular velocity to the array that was initlized before
 			circVelos = np.append(circVelos, circVelo)
 
 		return circVelos
 
 	def CircularVelocityTotal(self, radii):
-		pass
+		# INPUTS:
+		# 		radii -- array with various radius values (kpc)
+		#
+		# RETURNS:
+		# 		array total circular velocities within the given radii
+		darkMass, diskMass, bulgeMass = MassEnclosedTotal(radii)
+
+		totalMass = darkMass + diskMass + bulgeMass
+
+		return np.sqrt((G*totalMass)/radii)
+
+	def HernquistVCirc(self,r,a,darkMass):
+		# INPUTS:
+		#		radii -- 
+		# 		a -- scale length used in this profile
+		#		darkMass -- amount of dark matter to be used in the profile
+		#
+		# RETURNS:
+		# 		computed circular velocity according to the hernquist profile mass
+		# 		within the given radius
+
+
+		return np.around(np.sqrt((G*HernquistMass(radii,a,darkMass))/radii),2)
 
 
 
 r = np.arange(0.25, 30.5, 1.5); print(r)
 
+
+
 MW = MassProfile("MW", 0)
-print(MW.MassEnclosed(1,r))
-#print(test1.MassEnclosed(1,2))
+M31 = MassProfile("M31", 0)
+M33 = MassProfile("M33", 0)
+
+MWdarkMass, MWdiskMass, MWbulgeMass = MW.MassEnclosedTotal(r)
+M31darkMass, M31diskMass, M31bulgeMass = M31.MassEnclosedTotal(r)
+M33darkMass, M33diskMass, M33bulgeMass = M33.MassEnclosedTotal(r)
+
+MWtotalMass = MWdarkMass + MWdiskMass + MWbulgeMass
+M31totalMass = M31darkMass + M31diskMass + M31bulgeMass
+M33totalMass = M33darkMass + M33diskMass + M33bulgeMass
+
+
+fig,ax = plt.subplots(figsize=(10,8))
+
+
+#adjust tick label font size
+label_size = 22
+
+matplotlib.rcParams['xtick.labelsize'] = label_size 
+matplotlib.rcParams['ytick.labelsize'] = label_size
+
+# 
+plt.plot(r, MWdarkMass,linewidth = 5, label='MW dark matter',color="blue",linestyle="-")
+plt.plot(r, MWdiskMass,linewidth = 5, label='MW disk matter',color="blue",linestyle=":")
+plt.plot(r, MWbulgeMass,linewidth = 5, label='MW bulge matter',color="blue",linestyle="--")
+
+# 
+plt.plot(r, M31darkMass,linewidth = 5, label='M31 dark matter',color="red",linestyle="-")
+plt.plot(r, M31diskMass,linewidth = 5, label='M31 disk matter',color="red",linestyle=":")
+plt.plot(r, M31bulgeMass,linewidth = 5, label='M31 bulge matter',color="red",linestyle="--")
+
+# 
+plt.plot(r, M33darkMass,linewidth = 5, label='M33 dark matter',color="green",linestyle="-")
+plt.plot(r, M33diskMass,linewidth = 5, label='M33 disk matter',color="green",linestyle=":")
+
+
+
+
+
+# Continue plotting for the other redshifts here
+
+
+# Legend
+plt.legend(loc='lower right',fontsize='x-large')
+plt.show()
+
+
 
 
 
