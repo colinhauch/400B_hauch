078b7ac2c8deb07861cccb8725b141b512a4f63e
Colin Hauch <colinhauch@email.arizona.edu>
2020-04-23 10:56:20 -0500

    Rearranged directory system twice

--- Research/HauchProject/testNPYsave.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-data = np.load("LowResNPY/M31_004.npy")
-
-print(data)
--- Research/HauchProject/tester.py
+++
@@ -1,7 +0,0 @@
-import numpy as np 
-
-mylist = np.array([12,13,14,15,12,13,14,19])
-
-index = np.array([1,3,5])
-
-print(mylist[index])
